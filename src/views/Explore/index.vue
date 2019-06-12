8aef1127d469502667708e36532c9108307e2360
MohanRaj Balumuri <balumuriraj@gmail.com>
2019-06-11 17:17:01 -0700

    Removed unwanted files

--- src/views/Explore/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section class="content">
-    <!-- Outlet for children routes -->
-    <router-view></router-view>
-  </section>
-</template>
-
-<script lang="ts" src="./index.ts"></script>
-<style scoped lang="scss" src="./index.scss"></style>
--- src/views/Login/index.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import auth from '@/auth';
-
-@Component
-export default class Login extends Vue {
-  public mounted() {
-    auth.initUI('#firebaseui-auth-container', this);
-  }
-}
